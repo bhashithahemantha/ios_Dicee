b9509f29272bc412e849fb4d8405ab82df0d3cc3
bhashi <bhashithahemantha@gmail.com>
2017-11-23 17:30:20 +0530

    Initial commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by bhashitha hemantha on 11/23/17.
+//  Copyright © 2017 bhashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1:Int = 0
+    var randomDiceIndex2:Int = 0
+    @IBOutlet weak var result: UILabel!
+    
+    let diceArray = ["dice1","diec2","dice3","dice4","dice5","dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollPressed(_ sender: UIButton) {
+        
+        updateDiceImages()
+        if(randomDiceIndex1 == randomDiceIndex2){
+            
+            result.text = "correct...!"
+            
+        }else{
+            
+            result.text = "incorrect...!"
+            
+        }
+        
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateDiceImages()
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
+        result.text = "welcome...!"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
